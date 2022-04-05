33d520abd8465e4e5a3e270f173241cfc853a613
Inffinite <louislaizer4@gmail.com>
2022-04-05 22:20:56 +0300

    :poop: Fuck testing

--- app/src/routers/Users.js
+++ app/src/routers/Users.js
@@ -4,6 +4,7 @@ const auth = require('../middleware/auth')
 // const { sendMessage, generateCode } = require('../functions/sms')
 const router = new express.Router()
 
+
 router.post('/users', async (req, res) => {
     const user = new User(req.body)
 
--- app/src/test/general.test.js
+++
@@ -1,6 +0,0 @@
-const axios = require('axios')
-
-test('get /', async () => {
-    const url = 'http://app:3000'
-    await axios.get(url)
-})
